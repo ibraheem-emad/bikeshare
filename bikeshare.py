645a5a2901c63f32d9a546473fb51e9b088d873e
ibraheem-emad <ibraheemimad123@gmail.com>
2025-12-04 00:46:59 +0200

    second refactor

--- bikeshare.py
+++ bikeshare.py
@@ -46,7 +46,7 @@ def get_filters():
     print('-'*40)
     return city, month, day
 
-
+# comment 2
 def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
